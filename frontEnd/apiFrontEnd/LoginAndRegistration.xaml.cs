57fa559cb24277764e3181843da69f71b4c7b92c
ting <tli101@my.centennialcollege.ca>
2021-11-21 17:28:12 -0500

    login and registration working

--- frontEnd/apiFrontEnd/LoginAndRegistration.xaml.cs
+++ frontEnd/apiFrontEnd/LoginAndRegistration.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using apiFrontEnd.Models;
+using apiFrontEnd.StaticValues;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,16 +24,86 @@ namespace apiFrontEnd
     /// </summary>
     public partial class LoginAndRegistration : Window
     {
+        private readonly string _loginUrl = @"home/login";
+        private readonly string _registerUrl = @"home/Register";
+        private string _token;
+        private string _userName;
         public LoginAndRegistration()
         {
             InitializeComponent();
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             string username = UserNameTextBox_Login.Text;
             string password = PasswordTextBox_Login.Text;
-            
+
+            if(string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                ErrorMsgLable_Login.Content = "Please fill all field";
+            }
+
+            UserAuth user = new UserAuth();
+            user.UserName = username;
+            user.Password = password;
+            StringContent content = new StringContent(JsonConvert.SerializeObject(user), System.Text.Encoding.UTF8, "application/json");
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.PostAsync(BackEndConnection.BaseUrl + _loginUrl, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    UserAuth userReturn = JsonConvert.DeserializeObject<UserAuth>(response.Content.ReadAsStringAsync().Result);
+                    _token = userReturn.Token;
+                    _userName = userReturn.UserName;
+                    MainWindow mw = new MainWindow(_userName, _token);
+                    mw.Show();
+                    this.Close();
+                }
+                else
+                {
+                    JObject errorDic = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
+                    List<string> errorList = errorDic["errors"].ToObject<List<string>>();
+                    string error = string.Join(@"\n", errorList);
+                    ErrorMsgLable_Login.Content = error;
+                }
+            }
         }
+
+        private async void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UserNameTextBox_Register.Text;
+            string password = PasswordTextBox_Register.Text;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                ErrorMsgLable_Login.Content = "Please fill all field";
+            }
+
+            UserAuth user = new UserAuth();
+            user.UserName = username;
+            user.Password = password;
+            StringContent content = new StringContent(JsonConvert.SerializeObject(user), System.Text.Encoding.UTF8, "application/json");
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.PostAsync(BackEndConnection.BaseUrl + _registerUrl, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    UserAuth userReturn = JsonConvert.DeserializeObject<UserAuth>(response.Content.ReadAsStringAsync().Result);
+                    _token = userReturn.Token;
+                    _userName = userReturn.UserName;
+                    MainWindow mw = new MainWindow(_userName, _token);
+                    mw.Show();
+                    this.Close();
+                }
+                else
+                {
+                    JObject errorDic = JsonConvert.DeserializeObject<JObject>(response.Content.ReadAsStringAsync().Result);
+                    List<string> errorList = errorDic["errors"].ToObject<List<string>>();
+                    string error = string.Join(Environment.NewLine, errorList);
+                    ErrorMsgLable_Register.Content = error;
+                }
+            }
+        }
+
     }
 }
