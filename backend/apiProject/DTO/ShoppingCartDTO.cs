fe2cd9926e7d190ad55bed84c439fbfd624cc2b2
ting <tli101@my.centennialcollege.ca>
2021-11-22 17:25:58 -0500

    shoppingCartItem Json name

--- backend/apiProject/DTO/ShoppingCartDTO.cs
+++ backend/apiProject/DTO/ShoppingCartDTO.cs
@@ -2,15 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace apiProject.DTO
 {
     public class ShoppingCartDTO
     {
+        [JsonPropertyName("shopping_cart_items")]
         public IEnumerable<ShoppingCartItem> ShoppingCartItems { get; set; }
+        [JsonPropertyName("total_cost")]
         public double TotalCost { get; set; }
+        [JsonPropertyName("shipping_address")]
         public string ShippingAddress { get; set; }
+        [JsonPropertyName("user_name")]
         public string UserName { get; set; }
     }
 }
