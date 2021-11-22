3bcfa8d61b4810206d0a18659ea9a46cbb9d9bb6
ting <tli101@my.centennialcollege.ca>
2021-11-22 07:57:28 -0500

    add json name

--- backend/apiProject/Models/OrderDetails.cs
+++ backend/apiProject/Models/OrderDetails.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace apiProject.Models
@@ -12,11 +13,20 @@ namespace apiProject.Models
     public class OrderDetails
     {
         [Key]
+        [JsonPropertyName("order_id")]
         public long OrderId { get; set; }
         public string Status { get; set; }
+
+        [JsonPropertyName("total_cost")]
         public double TotalCost { get; set; }
+
+        [JsonPropertyName("shipping_address")]
         public string ShippingAddress { get; set; }
+
+        [JsonPropertyName("order_time")]
         public DateTime OrderTime { get; set; }
+
+        [JsonPropertyName("user_name")]
         public string UserName { get; set; }
 
     }
